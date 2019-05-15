82beb8498a982094f48aab4f8f6b33a0391639ce
olutola moses <olutolamoses@gmail.com>
2019-05-15 16:11:38 +0100

    out2

--- test/loanspec.js
+++ test/loanspec.js
@@ -7,15 +7,17 @@ chai.use(chaiHttp);
 chai.should();
 chai.request(app);
 // parent and it functions for req.body
-describe('User Authentication', () => {
-  it('should get all loans', (done) => {
-    chai.request(app)
-      .get('/api/v1/loans')
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.should.be.an('object');
+describe('get loans endpoint', () => {
+  describe('GET/ loans', () => {
+    it('should get all loans', (done) => {
+      chai.request(app)
+        .get('/api/v1/loans')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.should.be.an('object');
 
-        done();
-      });
+          done();
+        });
+    });
   });
 });
