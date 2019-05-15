b1d1f55089a01ba8c4439af0c8c57315307cb621
olutola moses <olutolamoses@gmail.com>
2019-05-15 18:16:29 +0100

    feature(repayment):add repayment endpoint [Start #165807428]

---
+++ server/model/repayments.js
@@ -0,0 +1,14 @@
+
+const repayments = [
+  {
+    id: 1,
+    createdOn: '1960 08 12',
+    loanId: 3,
+    amount: 500000.00,
+    monthlyInstallment: 5000.00,
+
+  },
+];
+
+
+export default repayments;
