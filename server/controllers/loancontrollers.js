a05e67a504eb8d000285106188038e95745aaec2
olutola moses <olutolamoses@gmail.com>
2019-05-15 14:25:53 +0100

    feature(getloans):create route,controllers/models [Finish #165807394]

--- server/controllers/loancontrollers.js
+++ server/controllers/loancontrollers.js
@@ -1,1 +1,13 @@
+import loans from '../model/loans';
 
+
+class LoanController {
+  static getallLoans(req, res) {
+    return res.status(200).json({
+      message: 'All loans retrieved sucesfully',
+      loans,
+    });
+  }
+}
+
+export default LoanController;
